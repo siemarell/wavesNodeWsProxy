d9a0c816fb55a87f219878fd5a016a49f8559fda
siem <siemarell@gmail.com>
2018-06-17 18:55:24 +0300

    utx type 4

--- src/nodeProxy.ts
+++ src/nodeProxy.ts
@@ -2,21 +2,8 @@ import {Observable} from "rxjs/internal/Observable";
 import {map, concatMap, publish, share, filter, tap} from "rxjs/operators";
 import {interval, Observer, Subject} from "rxjs";
 import * as request from 'request-promise';
-import {asleep} from './utils'
-import {async} from "rxjs/internal/scheduler/async";
 import {Subscription} from "rxjs/internal/Subscription";
 
-interface IStorage {
-    lastUtx: number,
-    addresses: Array<any>,
-    subjects: Array<any>
-}
-
-
-// export function getObservable(channel: string): Observable<string> {
-//     if (channel == 'utx') return sync.utxObs;
-//     return interval(1000).pipe(map(x => `${channel} ${x.toString()}`))
-// }
 
 export interface INodeProxy {
     getChannel(channel: string): Observable<any>
@@ -25,19 +12,8 @@ export interface INodeProxy {
 export class NodeProxy implements INodeProxy{
     private utxPool: Map<string, {}> = new Map();
     private utxSubscription: Subscription;
-
-    private _processUtxResponce = (utxs: Array<any>) => {
-        const updatedPool = new Map<string, {}>();
-        utxs.forEach(utx =>{
-            if (!this.utxPool.has(utx.signature)){
-                console.log(utx);
-                this.utxData.next(utx);
-            }
-            updatedPool.set(utx.signature, utx)
-        });
-    };
-
     private readonly utxData: Subject<any> = new Subject<any>();
+
     constructor(private nodeUrl: string, private pollInterval: number){
        this.utxSubscription = interval(this.pollInterval)
             .pipe(
@@ -45,18 +21,8 @@ export class NodeProxy implements INodeProxy{
                     return await this.getUTXs();
                 }),
             )
-            .subscribe(this._processUtxResponce)
-
-        // utxObservable.subscribe(utxSubject);
-        // this.utxData = utxSubject;
+            .subscribe(this._processUtxResponse)
     }
-
-    storage: IStorage = {
-        lastUtx: -1,
-        addresses: [],
-        subjects: []
-    };
-
 
     private async getUTXs(): Promise<Array<any>> {
         const options = {
@@ -66,14 +32,25 @@ export class NodeProxy implements INodeProxy{
         return await request.get(options);
     }
 
-    getChannel(channelName: string){
+    private _processUtxResponse = (utxs: Array<any>) => {
+        const updatedPool = new Map<string, {}>();
+
+        utxs.filter(utx => utx.type === 4)
+            .forEach(utx =>{
+                if (!this.utxPool.has(utx.signature)){
+                    console.log(utx);
+                    this.utxData.next(utx);
+                }
+                updatedPool.set(utx.signature, utx)
+            });
+
+        this.utxPool = updatedPool;
+    };
+
+    public getChannel(channelName: string){
         if (channelName === 'utx'){
             return this.utxData.asObservable()
         }
         return new Observable()
     }
 }
-
-// const proxy = new NodeProxy("https://nodes.wavesplatform.com", 3000);
-// proxy.getChannel('utx').subscribe(x=> console.log(x))
-// setInterval(console.log, 3000, "1")
